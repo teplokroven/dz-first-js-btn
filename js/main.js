a68c1045dab2d93f1bb2f0835a87052c1a2476be
Denis <chemnkss@gmail.com>
2025-06-17 16:09:42 +0300

    практика js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-const name = 'А я Денис';
 const btn = document.querySelector('.hero-btn');
 
-btn.addEventListener('click', function() {
-  console.log(name);
-})
+if (btn.textContent == 'Скрыть') {
+  btn.textContent = 'Показать все';
+} else {
+  btn.textContent = 'Да';
+}
